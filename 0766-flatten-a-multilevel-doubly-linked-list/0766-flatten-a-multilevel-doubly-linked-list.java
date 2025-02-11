ce1219311e9ec48f1d2ce360d494f02fbfe64a19
NitikaSheoran <nitikasheoran5@gmail.com>
2025-02-11 11:43:22 +0530

    Time: 0 ms (100%), Space: 41.9 MB (45.55%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -14,14 +14,10 @@ class Solution {
             return head;
         }
         Node temp = head;
-        int i=0;
         while(temp.next != null || temp.child!=null){
             Node x = temp;
             Node y = temp.next;
-            // System.out.println("iteration "+i + " x.val = "+x.val + "x.next = "+ y.val);
-            i++;
             if(temp.child != null){
-                // System.out.println("child found at node value : "+temp.val +" child = "+temp.child.val);
                 temp.child.prev=temp;
                 temp.next = temp.child;
                 temp.child = null;
@@ -34,15 +30,8 @@ class Solution {
                     y.prev=temp;
                 }
             }
-            // System.out.println("list updated: ");
             temp = x;
             temp = temp.next;
-        }
-        // System.out.println("final ll: ");
-        temp=head;
-        while(temp!=null){
-            System.out.print(temp.val + "  ");
-            temp=temp.next;
         }
         return head;
     }
