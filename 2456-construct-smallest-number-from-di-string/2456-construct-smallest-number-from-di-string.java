1a0d814124a60823040f6c7105db05e16bd38c6d
NitikaSheoran <nitikasheoran5@gmail.com>
2025-02-18 19:17:05 +0530

    Time: 1 ms (75.25%), Space: 41.6 MB (62.62%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,28 +1,16 @@
 class Solution {
 
     public String smallestNumber(String pattern) {
-        return String.valueOf(findSmallestNumber(pattern, 0, 0, 0));
-    }
-    private int findSmallestNumber(
-        String pattern,
-        int currentPosition,
-        int usedDigitsMask,
-        int currentNum
-    ) {
-        if (currentPosition > pattern.length()) return currentNum;
-
-        int result = Integer.MAX_VALUE;
-        int lastDigit = currentNum % 10;
-        boolean shouldIncrement = currentPosition == 0 || pattern.charAt(currentPosition - 1) == 'I';
-
-        for (int currentDigit = 1; currentDigit <= 9; ++currentDigit) {
-            if ((usedDigitsMask & (1 << currentDigit)) == 0 && currentDigit > lastDigit == shouldIncrement){
-                result = Math.min(result, findSmallestNumber(pattern, currentPosition + 1,
-                    usedDigitsMask | (1 << currentDigit),
-                    currentNum * 10 + currentDigit));
+        StringBuilder result = new StringBuilder();
+        Stack<Integer> numStack = new Stack<>();
+        for (int index = 0; index <= pattern.length(); index++) {
+            numStack.push(index + 1);
+            if (index == pattern.length() || pattern.charAt(index) == 'I') {
+                while (!numStack.isEmpty()) {
+                    result.append(numStack.pop());
                 }
+            }
         }
-
-        return result;
+        return result.toString();
     }
 }
