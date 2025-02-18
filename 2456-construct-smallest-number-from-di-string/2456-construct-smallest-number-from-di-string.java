47033205aa4d350bbbe39493e5a78d7c43e6531b
NitikaSheoran <nitikasheoran5@gmail.com>
2025-02-18 19:14:32 +0530

    Time: 13 ms (11.64%), Space: 41.6 MB (76.89%) - LeetHub

--- 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
+++ 2456-construct-smallest-number-from-di-string/2456-construct-smallest-number-from-di-string.java
@@ -1,70 +1,28 @@
 class Solution {
-    private boolean check(String sequence, String pattern) {
-        for (int index = 0; index < pattern.length(); index++) {
-            if (pattern.charAt(index) == 'I') {
-                if (sequence.charAt(index) > sequence.charAt(index + 1)) {
-                    return false;
-                }
-            } else {
-                if (sequence.charAt(index) < sequence.charAt(index + 1)) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 
     public String smallestNumber(String pattern) {
-        int patternLength = pattern.length();
-        char[] numberSequence = new char[patternLength + 1];
-        for (int position = 0; position <= patternLength; position++) {
-            numberSequence[position] = (char) ('1' + position);
-        }
-        while (!check(new String(numberSequence), pattern)) {
-            if (!findNextPermutation(numberSequence)) {
-                break;
-            }
-        }
-
-        return new String(numberSequence);
+        return String.valueOf(findSmallestNumber(pattern, 0, 0, 0));
     }
+    private int findSmallestNumber(
+        String pattern,
+        int currentPosition,
+        int usedDigitsMask,
+        int currentNum
+    ) {
+        if (currentPosition > pattern.length()) return currentNum;
 
-    private boolean findNextPermutation(char[] numberSequence) {
-        int lastIncreasingIndex = numberSequence.length - 2;
-        while (
-            lastIncreasingIndex >= 0 &&
-            numberSequence[lastIncreasingIndex] >=
-            numberSequence[lastIncreasingIndex + 1]
-        ) {
-            lastIncreasingIndex--;
-        }
-        if (lastIncreasingIndex == -1) return false;
-        int swapIndex = numberSequence.length - 1;
-        while (
-            numberSequence[swapIndex] <= numberSequence[lastIncreasingIndex]
-        ) {
-            swapIndex--;
-        }
-        swapCharacters(numberSequence, lastIncreasingIndex, swapIndex);
-        reverseSuffix(
-            numberSequence,
-            lastIncreasingIndex + 1,
-            numberSequence.length - 1
-        );
-        return true;
-    }
+        int result = Integer.MAX_VALUE;
+        int lastDigit = currentNum % 10;
+        boolean shouldIncrement = currentPosition == 0 || pattern.charAt(currentPosition - 1) == 'I';
 
-    private void swapCharacters(char[] array, int firstIdx, int secondIdx) {
-        char temp = array[firstIdx];
-        array[firstIdx] = array[secondIdx];
-        array[secondIdx] = temp;
-    }
-
-    private void reverseSuffix(char[] array, int startIdx, int endIdx) {
-        while (startIdx < endIdx) {
-            swapCharacters(array, startIdx, endIdx);
-            startIdx++;
-            endIdx--;
+        for (int currentDigit = 1; currentDigit <= 9; ++currentDigit) {
+            if ((usedDigitsMask & (1 << currentDigit)) == 0 && currentDigit > lastDigit == shouldIncrement){
+                result = Math.min(result, findSmallestNumber(pattern, currentPosition + 1,
+                    usedDigitsMask | (1 << currentDigit),
+                    currentNum * 10 + currentDigit));
+                }
         }
+
+        return result;
     }
 }
