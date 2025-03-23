63749360c0e20a449aa9b10a644102d12074af2e
NitikaSheoran <nitikasheoran5@gmail.com>
2025-03-23 21:46:33 +0530

    Time: 15 ms (20.44%), Space: 49.6 MB (40.6%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
@@ -1,52 +1,48 @@
 class Solution {
-
     public int countPaths(int n, int[][] roads) {
-        final int MOD = 1_000_000_007;
         List<List<int[]>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+        
         for (int[] road : roads) {
-            int startNode = road[0], endNode = road[1], travelTime = road[2];
-            graph.get(startNode).add(new int[] { endNode, travelTime });
-            graph.get(endNode).add(new int[] { startNode, travelTime });
+            int u = road[0], v = road[1], time = road[2];
+            graph.get(u).add(new int[]{v, time});
+            graph.get(v).add(new int[]{u, time});
         }
-        PriorityQueue<long[]> minHeap = new PriorityQueue<>(
-            Comparator.comparingLong(a -> a[0])
-        );
-        long[] shortestTime = new long[n];
-        Arrays.fill(shortestTime, Long.MAX_VALUE);
-        int[] pathCount = new int[n];
 
-        shortestTime[0] = 0; 
-        pathCount[0] = 1; 
+        long[] dist = new long[n];
+        int[] ways = new int[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
+        dist[0] = 0;
+        ways[0] = 1;
 
-        minHeap.offer(new long[] { 0, 0 });
+        PriorityQueue<long[]> pq = new PriorityQueue<>(Comparator.comparingLong(a -> a[0]));
+        pq.offer(new long[]{0, 0});
 
-        while (!minHeap.isEmpty()) {
-            long[] top = minHeap.poll();
-            long currTime = top[0]; 
-            int currNode = (int) top[1];
-            if (currTime > shortestTime[currNode]) {
-                continue;
-            }
+        int MOD = 1_000_000_007;
 
-            for (int[] neighbor : graph.get(currNode)) {
-                int neighborNode = neighbor[0], roadTime = neighbor[1];
-                if (currTime + roadTime < shortestTime[neighborNode]) {
-                    shortestTime[neighborNode] = currTime + roadTime;
-                    pathCount[neighborNode] = pathCount[currNode];
-                    minHeap.offer(
-                        new long[] { shortestTime[neighborNode], neighborNode }
-                    );
-                }
-                else if (currTime + roadTime == shortestTime[neighborNode]) {
-                    pathCount[neighborNode] =
-                        (pathCount[neighborNode] + pathCount[currNode]) % MOD;
+        while (!pq.isEmpty()) {
+            long[] curr = pq.poll();
+            long d = curr[0];
+            int node = (int) curr[1];
+
+            if (d > dist[node]) continue;
+
+            for (int[] neighbor : graph.get(node)) {
+                int nextNode = neighbor[0];
+                int time = neighbor[1];
+
+                if (dist[node] + time < dist[nextNode]) {
+                    dist[nextNode] = dist[node] + time;
+                    ways[nextNode] = ways[node];
+                    pq.offer(new long[]{dist[nextNode], nextNode});
+                } else if (dist[node] + time == dist[nextNode]) {
+                    ways[nextNode] = (ways[nextNode] + ways[node]) % MOD;
                 }
             }
         }
 
-        return pathCount[n - 1];
+        return ways[n - 1];
     }
 }
