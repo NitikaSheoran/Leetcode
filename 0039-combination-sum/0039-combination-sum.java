b112989bcef86eeed41e13fbd01b74b6ce927e7b
NitikaSheoran <nitikasheoran5@gmail.com>
2025-05-29 17:50:24 +0530

    Time: 2 ms (83.55%), Space: 44.9 MB (32.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,23 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    public void comb(int[] candidates, int idx, int sum, List<Integer> arr, int target){
+    public void comb(int[] nums, int i, int sum, int target, List<List<Integer>> res, List<Integer> arr){
         if(sum == target){
             res.add(new ArrayList<>(arr));
             return;
         }
-        if(sum > target || idx == candidates.length){
+        if(i == nums.length || sum>target){
             return;
         }
-        for(int i=idx; i<candidates.length; i++){
-            arr.add(candidates[i]);
-            comb(candidates, i, sum+candidates[i], arr, target);
-            arr.remove(arr.size()-1);
-        }
+        
+        arr.add(nums[i]);
+        comb(nums, i, sum+nums[i], target, res, arr);
+        
+        arr.remove(arr.size()-1);
+        comb(nums, i+1, sum, target, res, arr);
     }
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> arr = new ArrayList<>();
-        comb(candidates, 0, 0, arr, target);
+        comb(candidates, 0, 0, target, res, arr);
         return res;
     }
 }
