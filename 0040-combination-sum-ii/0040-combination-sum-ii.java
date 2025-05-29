db6172bb7dcbaa3cece8775c96cec1b69eb8d01b
NitikaSheoran <nitikasheoran5@gmail.com>
2025-05-29 18:25:35 +0530

    Time: 5 ms (18.54%), Space: 43.2 MB (72.11%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,27 +1,27 @@
-import java.util.*;
-
 class Solution {
-    List<List<Integer>> res = new ArrayList<>();
-    
-    public void comb(int[] candidates, int sum, List<Integer> arr, int target, int idx) {
-        if (sum == target) {
+    public void comb(int[] nums, int i, int sum, int target, List<List<Integer>> res, List<Integer> arr){
+        if(sum == target){
             res.add(new ArrayList<>(arr));
             return;
         }
-        if (sum > target) return;
 
-        for (int i = idx; i < candidates.length; i++) {
-            if (i > idx && candidates[i] == candidates[i - 1]) continue; // Skip duplicate elements
-            
-            arr.add(candidates[i]);
-            comb(candidates, sum + candidates[i], arr, target, i + 1);
-            arr.remove(arr.size() - 1);
+        if(sum > target){
+            return;
+        }
+
+        for(int index = i; index<nums.length; index++){
+            if(index > i && nums[index] == nums[index-1]) continue;
+
+            arr.add(nums[index]);
+            comb(nums, index+1, sum+nums[index], target, res, arr);
+            arr.remove(arr.size()-1);
         }
     }
-    
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates); // Sorting helps handle duplicates
-        comb(candidates, 0, new ArrayList<>(), target, 0);
+        Arrays.sort(candidates);
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        comb(candidates, 0, 0, target, res, arr);
         return res;
     }
 }
