1d45cd8fe435dfe52b3dda7d70c39a464d168760
NitikaSheoran <nitikasheoran5@gmail.com>
2025-05-29 17:34:51 +0530

    Time: 0 ms (100%), Space: 43.1 MB (18.03%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,18 +1,18 @@
 class Solution {
-    public List<List<Integer>> res = new ArrayList<>();
-    public void comb(List<Integer> arr, int[] nums, int i){
-        if(i==nums.length){
+    public void sub(int[] nums, int i, List<List<Integer>> res, List<Integer> arr){
+        if(i == nums.length){
             res.add(new ArrayList<>(arr));
             return;
         }
         arr.add(nums[i]);
-        comb(arr, nums, i+1);
+        sub(nums, i+1, res, arr);
         arr.remove(arr.size()-1);
-        comb(arr, nums, i+1);
+        sub(nums, i+1, res, arr);
     }
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
         List<Integer> arr = new ArrayList<>();
-        comb(arr, nums, 0);
+        sub(nums, 0, res, arr);
         return res;
     }
 }
