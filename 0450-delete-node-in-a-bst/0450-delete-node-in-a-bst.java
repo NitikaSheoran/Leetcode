a49b25c3d5011abbbc31e4640faf9d9d1366b478
NitikaSheoran <nitikasheoran5@gmail.com>
2025-06-08 17:04:38 +0530

    Time: 0 ms (100%), Space: 46 MB (77.36%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -18,20 +18,23 @@ class Solution {
         if (root == null) {
             return null;
         }
-
+        // find the node
         if(root.val<key){
             root.right=deleteNode(root.right,key);
         }else if(root.val>key){
             root.left=deleteNode(root.left,key);
         }else{
+            // if node is child node
             if(root.left==null&&root.right==null){
                 return null;
             }
+            // if it has 1 child, replace it with that child
             if(root.left==null){
                 return root.right;
             }else if(root.right==null){
                 return root.left;
             }
+            // if it has both children, find inordersuccessor of root.right an replace the data, delete the inorder successor
             TreeNode IS=findInorderSuccessor(root.right);
             root.val=IS.val;
             root.right=deleteNode(root.right,IS.val);
