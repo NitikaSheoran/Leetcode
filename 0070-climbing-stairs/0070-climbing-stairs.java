6cbf7f9b7046e21966925bb4edad30981b3ba92f
NitikaSheoran <nitikasheoran5@gmail.com>
2025-06-21 01:40:43 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,34 @@
 class Solution {
-    int[] dp;
-    public int climb(int n, int i, int[] dp){
-        if(i == n){
-            return 1;
-        }
-        if(i > n){
-            return 0;
-        }
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        return dp[i] = climb(n, i+1, dp) + climb(n, i+2, dp);
-    }
+    // memoization
+
+    // int[] dp;
+    // public int climb(int n, int i, int[] dp){
+    //     if(i == n){
+    //         return 1;
+    //     }
+    //     if(i > n){
+    //         return 0;
+    //     }
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+    //     return dp[i] = climb(n, i+1, dp) + climb(n, i+2, dp);
+    // }
+    // public int climbStairs(int n) {
+    //     dp = new int[n+1];
+    //     Arrays.fill(dp, -1);
+    //     return climb(n,0, dp);
+    // }
+
+    // tabulation
     public int climbStairs(int n) {
-        dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return climb(n,0, dp);
+        int[] dp = new int[n+2];
+        dp[n] = 1;
+        dp[n+1] = 0;
+        for(int i=n-1; i>=0; i--){
+            dp[i] = dp[i+1] + dp[i+2];
+        }
+        return dp[0];
     }
+
 }
