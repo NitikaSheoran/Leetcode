c7ce88439146219f155140a780d51b9ffdb22ecd
NitikaSheoran <nitikasheoran5@gmail.com>
2025-06-21 01:48:07 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.58%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -21,14 +21,30 @@ class Solution {
     // }
 
     // tabulation
-    public int climbStairs(int n) {
-        int[] dp = new int[n+2];
-        dp[n] = 1;
-        dp[n+1] = 0;
+    // public int climbStairs(int n) {
+    //     int[] dp = new int[n+2];
+    //     dp[n] = 1;
+    //     dp[n+1] = 0;
+    //     for(int i=n-1; i>=0; i--){
+    //         dp[i] = dp[i+1] + dp[i+2];
+    //     }
+    //     return dp[0];
+    // }
+
+    // space optimise
+     public int climbStairs(int n) {
+        int next = 1;
+        int secnext = 0;
+        int res = 0;
         for(int i=n-1; i>=0; i--){
-            dp[i] = dp[i+1] + dp[i+2];
+            res = next + secnext;
+            secnext = next;
+            next = res;
+            
         }
-        return dp[0];
+        return next;
     }
+
+
 
 }
