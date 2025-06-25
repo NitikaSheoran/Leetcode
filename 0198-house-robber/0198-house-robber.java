4064f161d28beb2d19c658a74d23938f6295bc90
NitikaSheoran <nitikasheoran5@gmail.com>
2025-06-26 02:37:59 +0530

    Time: 0 ms (100%), Space: 40.8 MB (88.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -17,18 +17,36 @@ class Solution {
     // }
 
     // memoization 
-    public int f(int i, int n, int[] nums, int[] dp){
-        if(i < 0) return 0;
-        if(dp[i] != -1) return dp[i];
+    // public int f(int i, int n, int[] nums, int[] dp){
+    //     if(i < 0) return 0;
+    //     if(dp[i] != -1) return dp[i];
 
-        int rob = nums[i] + f(i-2, n, nums, dp);
-        int skip = f(i-1, n, nums, dp);
-        return dp[i] = Math.max(rob, skip);
-    }
-    public int rob(int[] nums) {
+    //     int rob = nums[i] + f(i-2, n, nums, dp);
+    //     int skip = f(i-1, n, nums, dp);
+    //     return dp[i] = Math.max(rob, skip);
+    // }
+    // public int rob(int[] nums) {
+    //     int n = nums.length;
+    //     int[] dp = new int[n+1];
+    //     Arrays.fill(dp, -1);
+    //     return f(n-1, n, nums, dp);
+    // }
+
+    // tabulation
+    public int rob(int[] nums){
+        if(nums.length == 1) return nums[0];
         int n = nums.length;
         int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return f(n-1, n, nums, dp);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2; i<n; i++){
+            int rob = dp[i-2] + nums[i];
+            int skip = dp[i-1];
+            dp[i] = Math.max(rob, skip);
+        }
+
+        return dp[n-1];
     }
+
+
 }
