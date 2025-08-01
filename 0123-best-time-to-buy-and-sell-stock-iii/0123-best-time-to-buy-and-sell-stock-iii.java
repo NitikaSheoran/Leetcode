1667cee3de1f717cdfdcaf3cbea77763bb758b31
NitikaSheoran <nitikasheoran5@gmail.com>
2025-08-02 00:15:14 +0530

    Time: 62 ms (40%), Space: 63.9 MB (41.84%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,66 @@
+class Solution {
+    // recursive
+
+    // public int f(int[] prices, int i, int buy, int cap){
+    //     if(cap == 0) return 0;
+    //     if(i == prices.length) return 0;
+
+    //     if(buy == 1){
+    //         return Math.max(-prices[i] + f(prices, i+1, 0, cap), 0 + f(prices, i+1, 1, cap));
+    //     }else{
+    //         return Math.max(prices[i] + f(prices, i+1, 1, cap-1), 0 + f(prices, i+1, 0, cap));
+    //     }
+    // }
+
+
+    // memo
+    // public int f(int[] prices, int i, int buy, int cap, int[][][] dp){
+    //     if(cap == 0) return 0;
+    //     if(i == prices.length) return 0;
+    //     if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
+
+    //     if(buy == 1){
+    //         return dp[i][buy][cap] = Math.max(-prices[i] + f(prices, i+1, 0, cap, dp), 0 + f(prices, i+1, 1, cap, dp));
+    //     }else{
+    //         return dp[i][buy][cap] = Math.max(prices[i] + f(prices, i+1, 1, cap-1, dp), 0 + f(prices, i+1, 0, cap, dp));
+    //     }
+    // }
+    public int maxProfit(int[] prices) {
+        int[][][] dp = new int[prices.length+1][2][3];
+        for(int i=0; i<=prices.length; i++){
+            for(int j=0; j<2; j++){
+                Arrays.fill(dp[i][j], -1);
+            }
+        }
+
+        // return f(prices, 0, 1, 2, dp);
+        int n = prices.length;
+
+        for(int i=0; i<n; i++){
+            for(int buy=0; buy<2; buy++){
+                dp[i][buy][0] = 0;
+            }
+        }
+        for(int buy=0; buy<2; buy++){
+            for(int cap=0; cap<3; cap++){
+                dp[n][buy][cap] = 0;
+            }
+        }
+
+
+        for(int i=n-1; i>=0; i--){
+            for(int buy=0; buy<2; buy++){
+                for(int cap=1; cap<=2; cap++){
+                    if(buy == 1){
+                        dp[i][buy][cap] = Math.max(-prices[i] + dp[i+1][0][cap], 0 + dp[i+1][1][cap]);
+                    }else{
+                        dp[i][buy][cap] = Math.max(prices[i] + dp[i+1][1][cap-1], 0 + dp[i+1][0][cap]);
+                    }
+                }
+            }
+        }
+
+        return dp[0][1][2];
+
+    }
+}
