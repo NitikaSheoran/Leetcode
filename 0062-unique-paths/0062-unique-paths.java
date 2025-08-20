9e2e89934914a91e4701cfff5e10b860497c504a
NitikaSheoran <nitikasheoran5@gmail.com>
2025-08-20 08:23:08 +0530

    Time: 0 ms (100%), Space: 40.4 MB (77.29%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,53 +1,23 @@
 class Solution {
-    // recursion
-    // public int paths(int i, int j){
-    //     if(i == 0 && j == 0) return 1;
-    //     if(i<0 || j<0) return 0;
-    //     int up = paths(i-1, j);
-    //     int left = paths(i, j-1);
-    //     return up+left;
-    // }
-
-
-    // memoization
-    // public int paths(int i, int j, int[][] dp){
-    //     if(i == 0 && j == 0) return 1;
-    //     if(i<0 || j<0) return 0;
-
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     int up = paths(i-1, j, dp);
-    //     int left = paths(i, j-1, dp);
-
-    //     return dp[i][j] = up+left;
-    // }
-
-
-    public int uniquePaths(int m, int n) {
-        // return paths(m-1, n-1);
-
-        // int[][] dp = new int[m][n];
-        // for(int i=0; i<m; i++){
-        //     Arrays.fill(dp[i], -1);
-        // }
-        // dp[0][0] = 1;
-        // paths(m-1, n-1, dp);
-        // return dp[m-1][n-1];
-
-
-        // tabulation
-        int[][] dp = new int[m][n];
-        dp[0][0] = 1;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(i == 0 && j == 0) continue;
-                int up = 0;
-                int left = 0;
-                if(i>=1) up = dp[i-1][j];
-                if(j>=1) left = dp[i][j-1];
-
-                dp[i][j] = up+left;
-            }
+    public int f(int i, int j, int[][] dp){
+        if(i == 0 && j == 0){
+            return 1;
         }
-        return dp[m-1][n-1];
+
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = f(i-1, j, dp);
+        int left = f(i, j-1, dp);
+
+        return dp[i][j] = up+left;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int[] row: dp){
+            Arrays.fill(row, -1);
+        }
+        return f(m-1, n-1, dp);
     }
 }
