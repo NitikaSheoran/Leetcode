0ce5e88320a5b870c3f8850588fad6e7185a2b5e
NitikaSheoran <nitikasheoran5@gmail.com>
2025-08-20 08:19:33 +0530

    Time: 4 ms (62.02%), Space: 50.5 MB (27.34%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,52 +1,20 @@
 class Solution {
-    public List<Integer> majorityElement(int[] arr) {
-        int count1 =0;
-        int count2 = 0;
-        int element1 = Integer.MIN_VALUE;
-        int element2 = Integer.MIN_VALUE;
-
-        List<Integer> list = new ArrayList<>();
-
-        for(int i=0; i<arr.length; i++){
-            if(count1 == 0 && arr[i] != element2){
-                element1 = arr[i];
-                count1++;
-            }
-            else if(count2 ==0 && arr[i] != element1){
-                element2 = arr[i];
-                count2++;                
-            }
-            else if(arr[i] == element1){
-                count1++;
-            }
-            else if(arr[i] == element2){
-                count2++;
-            }
-            else{
-                count1--;
-                count2--;
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<Integer> res = new ArrayList<>();
+        
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i < n && nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                if (count > n / 3) {
+                    res.add(nums[i - 1]);
+                }
+                count = 1;
             }
         }
-
-        count1 =0;
-        count2 =0;
-
-        for(int i=0; i<arr.length; i++){
-            if(arr[i] == element1){
-                count1++;
-            }
-            if(arr[i] == element2){
-                count2++;
-            }
-        }
-
-        if(count1 > arr.length/3){
-            list.add(element1);
-        }
-        if(count2 > arr.length/3 && element2 != element1){
-            list.add(element2);
-        }
-
-        return list;
+        return res;
     }
 }
