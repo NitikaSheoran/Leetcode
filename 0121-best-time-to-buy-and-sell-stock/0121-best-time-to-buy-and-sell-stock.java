c5b6904bcf23ba84ad08b0f7756aaa032fca63d0
NitikaSheoran <nitikasheoran5@gmail.com>
2026-04-28 13:52:08 +0530

    Time: 49 ms (1.48%), Space: 88.3 MB (99.26%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,15 +1,35 @@
 class Solution {
-    public int maxProfit(int[] prices) {
-        
-        int n=prices.length;
-        int maxDiff = Integer.MIN_VALUE;
-        int minPrice = Integer.MAX_VALUE;
-        for(int i=0;i<n; i++){
-            if(prices[i]<minPrice){
-                minPrice = prices[i];
+    public int[] nextLargest(int[] prices){
+        Stack<Integer> stack = new Stack<>();
+        int n = prices.length;
+        int[] res = new int[n];
+        res[n-1] = n;
+        stack.push(n-1);
+        int l = n-2;
+        while(!stack.isEmpty() && l>=0){
+            int top = stack.peek();
+            while(!stack.isEmpty() && prices[stack.peek()]<prices[l]){
+                stack.pop();
             }
-            maxDiff = Math.max(maxDiff, prices[i]-minPrice);
+            if(stack.isEmpty()){
+                res[l] = n;
+                stack.push(l);
+            }else{
+                res[l] = stack.peek();
+            }
+        
+            l--;
         }
-        return maxDiff;
+        return res;
+    }
+    public int maxProfit(int[] prices) {
+        int[] nextL = nextLargest(prices);
+        int n = prices.length;
+        int maxP = 0;
+        for(int i=0; i<n; i++){
+            if(nextL[i] != n)
+                maxP = Math.max(prices[nextL[i]]-prices[i], maxP);
+        }
+        return maxP;
     }
 }
