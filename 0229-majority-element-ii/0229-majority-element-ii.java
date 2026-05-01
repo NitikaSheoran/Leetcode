602630362f62070b9c9404af68417d5d8113f7a2
NitikaSheoran <nitikasheoran5@gmail.com>
2026-05-02 00:02:43 +0530

    Time: 2 ms (99.89%), Space: 52.6 MB (52.12%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,49 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        int maj1 = 0;
+        int maj2 = 0;
+        int count1 = 0;
+        int count2 = 0;
         int n = nums.length;
-        Arrays.sort(nums);
-        List<Integer> res = new ArrayList<>();
-        
-        int count = 1;
-        for (int i = 1; i <= n; i++) {
-            if (i < n && nums[i] == nums[i - 1]) {
-                count++;
-            } else {
-                if (count > n / 3) {
-                    res.add(nums[i - 1]);
-                }
-                count = 1;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == maj1){
+                count1++;
+            }else if(nums[i] == maj2){
+                count2++;
+            }else if(count1 == 0){
+                maj1 = nums[i];
+                count1 = 1;
+            }else if(count2 == 0){
+                maj2 = nums[i];
+                count2 = 1;
+            }else{
+                count1--;
+                count2--;
             }
         }
+
+        count1 = 0;
+        count2 = 0;
+        int thresh = n/3;
+
+        for(int i=0; i<n; i++){
+            if(nums[i] == maj1){
+                count1++;
+            }else if(nums[i] == maj2){
+                count2++;
+            }
+        }
+        List<Integer> res = new ArrayList<>();
+        if(count1 > n/3){
+            res.add(maj1);
+        }
+
+        if(count2 > n/3){
+            res.add(maj2);
+        }
         return res;
+        
+
     }
 }
