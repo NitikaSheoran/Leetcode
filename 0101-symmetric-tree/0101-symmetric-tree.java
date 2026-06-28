6cd69473492bf49d0687c1dda1cd8843d5cd7898
NitikaSheoran <nitikasheoran5@gmail.com>
2026-06-28 16:43:49 +0530

    Time: 0 ms (100%), Space: 43.3 MB (84.77%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,15 +14,23 @@
  * }
  */
 class Solution {
-    public boolean symmetry(TreeNode left, TreeNode right){
-        if(left == null && right == null) return true;
-        if(left == null && right != null)  return false;
-        if(left != null && right == null) return false;
-        if(right.val != left.val) return false;
-        return symmetry(left.right, right.left) && symmetry(left.left ,right.right);
+    public boolean symm(TreeNode left, TreeNode right){
+        if(left == null && right == null){
+            return true;
+        }
+        if((left == null && right != null) || (left != null && right == null)){
+            return false;
+        }
+        if(right.val != left.val){
+            return false;
+        }
+        return symm(left.right, right.left) && symm(left.left, right.right);
     }
     public boolean isSymmetric(TreeNode root) {
-        if(root == null) return true;
-        return symmetry(root.left, root.right);
+        if(root == null){
+            return true;
+        }
+
+        return symm(root.left, root.right);
     }
 }
