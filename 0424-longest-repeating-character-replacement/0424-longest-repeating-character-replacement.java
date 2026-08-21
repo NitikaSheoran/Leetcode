c07a48fa70aab29fb7beeea7379e6b5636c62348
NitikaSheoran <nitikasheoran5@gmail.com>
2026-08-21 17:45:55 +0530

    Time: 32 ms (16.24%), Space: 47.4 MB (7.29%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,25 +1,26 @@
 class Solution {
-    // longest substring with atmost k unique chars
     public int characterReplacement(String s, int k) {
-        int n=s.length();
-        int l=0;
-        int r=0;
-        int maxLen=0;
-        int maxRepeat=0;
-        Map<Character, Integer> unique = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int maxRepeat = 0;
+        int len = 0;
+        int n = s.length();
+        HashMap<Character, Integer> map = new HashMap<>();
+
         while(r<n){
-            char ch = s.charAt(r);
-            unique.put(ch, unique.getOrDefault(ch,0)+1);
-            maxRepeat = Math.max(maxRepeat, unique.get(ch));
-            if(r-l+1 - maxRepeat > k){
-                unique.put(s.charAt(l), unique.get(s.charAt(l))-1);
-                if(unique.get(s.charAt(l)) <= 0) unique.remove(s.charAt(l));
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r),0)+1);
+            maxRepeat = Math.max(maxRepeat, map.get(s.charAt(r)));
+            while(r-l+1 - maxRepeat > k){
+                map.put(s.charAt(l), map.get(s.charAt(l))-1);
+                if(map.get(s.charAt(l))<=0){
+                    map.remove(s.charAt(l));
+                }
                 l++;
             }
-            
-            maxLen = Math.max(maxLen, r-l+1);
+
+            len = Math.max(r-l+1, len);
             r++;
         }
-        return maxLen;
+        return len;
     }
 }
