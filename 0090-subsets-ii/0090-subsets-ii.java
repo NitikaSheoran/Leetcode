eb62b4cb245a4be7479b6a213013cc53b7351bb0
NitikaSheoran <nitikasheoran5@gmail.com>
2026-08-23 12:29:03 +0530

    Time: 2 ms (99.74%), Space: 45 MB (60.19%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,22 +1,23 @@
 class Solution {
-    public void f(int i, int[] nums, List<Integer> arr, List<List<Integer>> res){
-       
+    public void comb(int[] nums, int i, List<Integer> arr, List<List<Integer>> res){
         res.add(new ArrayList<>(arr));
-           
 
-        for(int idx=i; idx<nums.length; idx++){
+        for(int idx = i; idx<nums.length; idx++){
             if(idx>i && nums[idx] == nums[idx-1]) continue;
 
             arr.add(nums[idx]);
-            f(idx+1, nums, arr, res);
+            comb(nums, idx+1, arr, res);
             arr.remove(arr.size()-1);
         }
+
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         List<Integer> arr = new ArrayList<>();
         Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
-        f(0, nums, arr, res);
+        
+        comb(nums, 0, arr, res);
+        
         return res;
     }
 }
