646347216430a6ec05081c7c2a1560ceb55b712b
NitikaSheoran <nitikasheoran5@gmail.com>
2026-09-18 17:04:23 +0530

    Time: 8 ms (24.98%), Space: 47.4 MB (21.2%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,41 @@
 class Solution {
+
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int[] inDegree = new int[numCourses];
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        List<List<Integer>> adjList = new ArrayList<>();
         for(int i=0; i<numCourses; i++){
             adjList.add(new ArrayList<>());
         }
+
         for(int i=0; i<prerequisites.length; i++){
-            int[] arr = prerequisites[i];
-            adjList.get(arr[0]).add(arr[1]);
+            int[] prereq = prerequisites[i];
+            adjList.get(prereq[0]).add(prereq[1]);
         }
 
         for(int i=0; i<adjList.size(); i++){
-            for(int adj: adjList.get(i)){
-                inDegree[adj]++;
+            for(int course: adjList.get(i)){
+                inDegree[course]++;
             }
         }
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> topo = new LinkedList<>();
+        int res = 0;
+
         for(int i=0; i<inDegree.length; i++){
             if(inDegree[i] == 0){
-                q.add(i);
+                topo.add(i);
             }
         }
-        int res=0;
-        while(!q.isEmpty()){
-            int top = q.peek();
+        while(!topo.isEmpty()){
+            int top = topo.peek();
             res++;
-            q.poll();
+            topo.poll();
+
             for(int adj: adjList.get(top)){
                 inDegree[adj]--;
-                if(inDegree[adj] == 0) q.add(adj);
+                if(inDegree[adj] == 0) topo.add(adj);
             }
         }
+
         return res == numCourses;
     }
 }
