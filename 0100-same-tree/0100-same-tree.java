73ffdba27276e36b7bea74d775e41d7e055dab3f
NitikaSheoran <nitikasheoran5@gmail.com>
2026-09-24 12:08:56 +0530

    Time: 0 ms (100%), Space: 43.1 MB (15.84%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,16 +14,16 @@
  * }
  */
 class Solution {
+    public boolean f(TreeNode p, TreeNode q){
+        if(p == null && q == null) return true;
+        if((p == null && q != null) || (p != null && q == null) || (p.val != q.val)){
+            return false;
+        }
+
+        
+        return f(p.right, q.right) && f(p.left, q.left);
+    }
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null){
-            return true;
-        }
-        if((p == null && q != null) || (p != null && q==null)){
-            return false;
-        }
-        if(p.val!=q.val){
-            return false;
-        }
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        return f(p,q);
     }
 }
