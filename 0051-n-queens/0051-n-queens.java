075ff57f25879608a481898c5b3ef62d2bd6186a
NitikaSheoran <nitikasheoran5@gmail.com>
2026-09-25 16:45:57 +0530

    Time: 9 ms (5.86%), Space: 52.6 MB (5.4%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,80 +1,88 @@
 class Solution {
-    public boolean isSafe(int row, int col, char[][] board){
-        // Check the same row
-        for (int i = 0; i < board.length; i++) {
-            if (board[row][i] == 'Q') {
-                return false;
-            }
-        }
-        
-        // Check the same column
-        for (int i = 0; i < board[0].length; i++) {
-            if (board[i][col] == 'Q') {
+    public boolean isValid(int row, int col, char[][] board){
+        // check row
+        for(int i=0; i<board.length; i++){
+            if(i!=col && board[row][i] == 'Q'){
                 return false;
             }
         }
 
-        // Check the upper left diagonal
-        int r = row;
-        int c = col;
-        while (r >= 0 && c >= 0) {
-            if (board[r][c] == 'Q') {
+        // check col
+        for(int i=0; i<board.length; i++){
+            if(i!=row && board[i][col]=='Q'){
                 return false;
             }
-            r--;
-            c--;
         }
 
-    
-        // Check the lower left diagonal
-        r = row;
-        c = col;
-        while (r < board.length && c >= 0) {
-            if (board[r][c] == 'Q') {
-                return false;
-            }
-            r++;
-            c--;
+        // upper left diag
+        int i = row-1;
+        int j = col-1;
+        while(i>=0 && j>=0){
+            if(board[i][j] == 'Q') return false;
+            i = i-1;
+            j = j-1;
+        }
+
+        i = row+1;
+        j = col+1;
+        while(i<board.length && j<board.length){
+            if(board[i][j] == 'Q') return false;
+            i++;
+            j++;
+        }
+
+        i=row+1;
+        j=col-1;
+        while(i<board.length && j>=0){
+            if(board[i][j] == 'Q') return false;
+            i++;
+            j--;
+        }
+
+        i=row-1;
+        j=col+1;
+        while(i>=0 && j<board.length){
+            if(board[i][j] == 'Q') return false;
+            i--;
+            j++;
         }
         return true;
     }
-    public void addBoard(char[][]board, List<List<String>> allBoards){
-        List<String> newBoard = new ArrayList<String>();
-        for(int i = 0;i<board.length;i++){
-            String row = "";
-            for(int j = 0;j<board[0].length;j++){
-                if(board[i][j] == 'Q'){
-                    row+='Q';
-                }else{
-                    row+='.';
-                }
+    public void addBoard(List<List<String>> boards, char[][] board){
+        ArrayList<String> arr = new ArrayList<>();
+
+        for(int i=0; i<board.length; i++){
+            String str = "";
+            for(int j=0; j<board.length; j++){
+                str+=board[i][j];
             }
-            newBoard.add(row);
+            arr.add(str);
         }
-        allBoards.add(newBoard);
+        boards.add(arr);
     }
-    public void nQueen(char[][]board, List<List<String>> allBoards, int col){
-        if(col == board.length){
-            addBoard(board, allBoards);
+    public void f(char[][] board, List<List<String>> boards, int col, int n){
+        if(col == n){
+            addBoard(boards, board);
             return;
         }
-        for(int idx = 0; idx<board.length; idx++){
-            if(isSafe(idx, col, board)){
-                board[idx][col] = 'Q';
-                nQueen(board, allBoards, col+1);
-                board[idx][col] = '.';
+
+        for(int i=0; i<n; i++){
+            if(isValid(i, col, board)){
+                board[i][col] = 'Q';
+                f(board, boards, col+1, n);
+                board[i][col] = '.';
             }
         }
     }
     public List<List<String>> solveNQueens(int n) {
-        List<List<String>> allBoards = new ArrayList<>();
+        List<List<String>> res = new ArrayList<>();
         char[][] board = new char[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
                 board[i][j] = '.';
             }
         }
-        nQueen(board, allBoards, 0);
-        return allBoards;
+        f(board, res, 0, n);
+        return res;
     }
 }
